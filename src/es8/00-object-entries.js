f981270ff2e311b83c67d1090cac67fec79177ad
Juan <juanpablo.munoz@uber.com>
2023-01-17 22:38:18 -0500

    es8: object entries and values, string functions, trailling comas, async functions

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,9 @@
+const countries = {
+    "MX": "Mexico",
+    "CO": "Colombia",
+    "CL": "Chile",
+    "PE": "Peru",
+}
+
+console.log(Object.entries(countries));
+
---
+++ src/es8/03-trailling-comas.js
@@ -0,0 +1,4 @@
+const ages = [24, 34, 25, 19, , , ];
+
+console.log(ages);
+console.log(ages.length);
